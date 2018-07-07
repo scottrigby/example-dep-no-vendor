e72fc673e4145b28a94a8212d8926f562c1d13dd
Scott Rigby <scott@r6by.com>
2018-07-07 12:05:05 -0400

    Example dependency to test goreleaser with dep

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
 func main() {
-	println("Hello release")
+	_, err := fmt.Println("Hello release")
+	if err != nil {
+		println(errors.Cause(err))
+	}
 }
